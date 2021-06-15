59eafaf37df5402507a6077ad8ff48e6ab8cee18
Dmitry Mishuto <dmitry.mishuto@celadon.ae>
2021-06-15 18:16:48 +0300

    add requirements.txt

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,30 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'last_login', 'date_joined'),
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name'),
+        }),
+        ('Permissions', {
+            'fields': ('is_superuser', 'is_staff', 'is_active', 'groups'),
+        }),
+    )
+    list_display = ('pk', 'email', 'is_superuser', 'last_login', 'date_joined')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('pk',)
+
+
+admin.site.register(User, CustomUserAdmin)
